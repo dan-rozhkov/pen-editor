a254d54e9b13f5e3d133607730d7ef3db688c7ef
Danil Rozhkov <daniilrozh95@gmail.com>
2026-07-13 17:31:53 +0300

    fix: resize rectangular constraints center guide

--- src/components/properties/ConstraintsSection.tsx
+++ src/components/properties/ConstraintsSection.tsx
@@ -145,7 +145,7 @@ export function ConstraintsSection({ node, onUpdate }: ConstraintsSectionProps) 
               <span>Pin bottom</span>
             </TooltipContent>
           </Tooltip>
-          <div className="absolute left-6 top-3 h-8 w-8 rounded-md border border-border-default" />
+          <div className="absolute left-6 top-4 h-6 w-8 rounded-md border border-border-default" />
           <div className="absolute left-1/2 top-1/2 h-2.5 w-px -translate-x-1/2 -translate-y-1/2 bg-text-muted" />
           <div className="absolute left-1/2 top-1/2 h-px w-2.5 -translate-x-1/2 -translate-y-1/2 bg-text-muted" />
         </div>
